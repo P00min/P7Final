fd1f91f4f9cd27060ecc98667f46ccdbaaceefd8
P00min <718780@student.dusd.net>
2024-05-24 15:10:16 -0700

    Box Colliders p2
    Added some box colliders

--- P7FinalMaze/Assets/Scripts/PlayerLook.cs
+++ P7FinalMaze/Assets/Scripts/PlayerLook.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
@@ -11,24 +12,18 @@ public class PlayerLook : MonoBehaviour
 
     void Start()
     {
-        // Lock the cursor to the center of the screen and make it invisible
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
 
     void Update()
     {
-        // Get the mouse input
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        // Rotate the player body horizontally
-        playerBody.Rotate(Vector3.up * mouseX);
-
-        // Rotate the camera vertically
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Clamp to prevent flipping
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
